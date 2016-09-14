3ca446e6981daf5af16f4d30b876e18ff0c21d49
Juliette TRAN <tran.juliette.a@gmail.com>
2016-09-14 17:29:15 +0800

    Adding overlay, commenting, manage multi jQuery elements

--- imageuplodify.js
+++ imageuplodify.js
@@ -9,12 +9,6 @@
 // window and document to slightly quicken the process.
 // To be sure that undefined is truly undefined (For ES3)
 ;(function($, window, document, undefined) {
-
-  /*
-  Options:
-  - Submit (auto, manual, form (default))
-
-  */
  
   window.addEventListener("dragover",function(e){
     e = e || event;
@@ -30,148 +24,168 @@
   $.fn.imageuplodify = function(opts) {
 
     // Override default option with user's if exist.
-    var settings = $.extend( {}, $.fn.imageuplodify.defaults, opts);
-    var self = this;
-
-    var dragbox = $(`
-    <div>
-      <i class='glyphicon glyphicon-plus'></i>
-      <div>
-        <span>Drag your file(s) here...</span>
-      </div>
-    </div>
-    `);
-    var icon = dragbox.find("i");
-    var imagesList = dragbox.find("div");
-    var span = dragbox.find("span");
-
-    // Dropbox CSS
-    dragbox.css("border", "2px dashed");
-    dragbox.css("min-height", "350px");
-    dragbox.css("text-align", "center");
-    dragbox.css("width", "500px");
-    dragbox.css("display", "flex");
-    dragbox.css("flex-direction", "column");
-    dragbox.css("justify-content", "350px");
-    dragbox.css("background-color", "rgba(242, 242, 242, 0.7)");
-
-    // Add icon CSS
-    icon.css("display", "block !important");
-    icon.css("font-size", "3em");
-    icon.css("text-align", "center");
-    icon.css("margin-top", "1em");
-
-    // Image list CSS
-    imagesList.css("display", "inline-block");
-    imagesList.css("padding", "1em 0");
-    dragbox.find("div div:nth-child(4n+1)").css("margin-right", "1.5em");
-    dragbox.find("div div:nth-child(4n+2)").css("margin-left", "1.5em");    
-
-
-    const readingFile = (file) => {
-
-      var fReader = new FileReader();
-
-      // Associated function to a ending load
-      fReader.onloadend = function (e) {
-        var imageCtn = $("<div><img><div>");
-        var image = imageCtn.find("img");
-
-        // Image container CSS
-        imageCtn.css("width", "100px");
-        imageCtn.css("height", "100px");
-        imageCtn.css("position", "relative");
-        imageCtn.css("overflow", "hidden");
-        imageCtn.css("margin-left", "1em");
-        imageCtn.css("margin-bottom", "1em");
-        imageCtn.css("float", "left");
-        imageCtn.css("border-radius", "1em");
-
-        // Image CSS
-        image.css("height", "100px");
-        image.css("left", "50%");
-        image.css("position", "absolute");
-        image.css("top", "50%");
-        image.css("transform", "translate(-50%, -50%)");
-        image.css("width", "auto");
-
-        image.attr("src", e.target.result);
-        span.hide();
-        imagesList.append(imageCtn);
-      };
-
-      fReader.readAsDataURL(file);
-    };
-
-    // Manage click event.
-    icon.on("click", function onClick(event) {
-      $(self).click();
-    });
-
-    dragbox.on("dragenter", function onDrop(event) {
-
-      // TODO: Debug log.
-      console.log("Dragenter event");
-
-      event.stopPropagation();
-      event.preventDefault();
-
-      // Montrer la fenetre et les autres elements.
-
-    });
-
-    dragbox.on("dragleave", function onDrop(event) {
-
-      // TODO: Debug log.
-      console.log("Dragleave event");
-
-      event.stopPropagation();
-      event.preventDefault();
-
-      // Cacher la fenetre et les autres elements.
-
-    });
-
-    dragbox.on("drop", function onDrop(event) {
-
-      // TODO: Debug log.
-      console.log("Drop event");
-
-      event.stopPropagation();
-      event.preventDefault();
-      var files = event.originalEvent.dataTransfer.files; // || event.originalEvent.target.files;
-      
-      for(var index = 0; index < files.length; ++index) {
-        readingFile(files[index]);
-      }
-    });
-
-
-    self.on("change", function() {
-
-      // TODO: Debug log.
-      console.log("Change event");
-
-      var files = this.files;
-
-      for(var index = 0; index < files.length; ++index) {
-        readingFile(files[index]);
-      }
-    });
-
-    this.hide();
-    dragbox.insertAfter(this);
-
-    /* END TEST */
+    const settings = $.extend( {}, $.fn.imageuplodify.defaults, opts);
 
     // Initialize every element.
-    /*
     this.each(function() {
 
+      const self = this;
+
+      let dragbox = $(`
+      <div class="well">
+        <div class="overlay">
+          <span class="panel panel-default"><i class="glyphicon glyphicon-cloud-upload"></i> Drop to upload</span>
+        </div>
+        <div class="images-list text-center">
+          <i class="glyphicon glyphicon-plus"></i>
+          <span>Drag your file(s) here...</span>
+        </div>
+      </div>
+      `);
+
+      let overlay = dragbox.find(".overlay");
+      let uploadIcon = dragbox.find(".overlay span i");
+      let uploadMsg = dragbox.find(".overlay span");
+
+      let imagesList = dragbox.find(".images-list");
+      let addIcon = dragbox.find(".images-list i");
+      let addMsg = dragbox.find(".images-list span");
+
+      // Dropbox CSS
+      dragbox.css("border", "2px dashed rgb(210, 210, 210)");
+      dragbox.css("position", "relative");
+      dragbox.css("min-height", "350px");
+      dragbox.css("width", "500px");
+      dragbox.css("display", "flex");
+      dragbox.css("padding", "0px");
+      dragbox.css("flex-direction", "column");
+      dragbox.css("text-align", "center");
+      dragbox.css("background-color", "white");
+
+      // Overlay CSS
+      overlay.css("z-index", "10000");
+      overlay.css("width", "100%");
+      overlay.css("height", "100%");
+      overlay.css("position", "absolute");
+      overlay.css("flex-direction", "column");
+      overlay.css("top", "0");
+      overlay.css("left", "0");
+      overlay.css("display", "none");
+      overlay.css("background-color", "rgba(242, 242, 242, 0.7)");
+      overlay.css("text-align", "center");
+
+      // Uploading message CSS
+      // uploadMsg.css("background-color", "rgba(30, 30, 30, 0.5)");
+      uploadMsg.css("height", "100px");
+      uploadMsg.css("margin", "auto");
+      uploadMsg.css("padding-top", "29px");
+      uploadMsg.css("width", "200px");
+      uploadMsg.css("font-size", "1.1em");
+
+      // Image list CSS
+      imagesList.css("display", "inline-block");
+
+      // Add icon CSS
+      addIcon.css("display", "block");
+      addIcon.css("font-size", "3em");
+      addIcon.css("text-align", "center");
+      addIcon.css("margin-top", "3em");
+      addIcon.css("padding-bottom", "20px");
+
+
+      const readingFile = (file) => {
+
+        var fReader = new FileReader();
+
+        // Associated function to a ending load
+        fReader.onloadend = function (e) {
+          var imageCtn = $("<div><img><div>");
+          var image = imageCtn.find("img");
+
+          // Image container CSS
+          imageCtn.css("width", "100px");
+          imageCtn.css("height", "100px");
+          imageCtn.css("position", "relative");
+          imageCtn.css("overflow", "hidden");
+          imageCtn.css("margin-left", "1em");
+          imageCtn.css("margin-bottom", "1em");
+          imageCtn.css("float", "left");
+          imageCtn.css("border-radius", "1em");
+
+          // Image CSS
+          image.css("height", "100px");
+          image.css("left", "50%");
+          image.css("position", "absolute");
+          image.css("top", "50%");
+          image.css("transform", "translate(-50%, -50%)");
+          image.css("width", "auto");
+          image.css("border", "inset 0px 0px 40px 40px rgba(186, 235, 245, 1)");
+
+          image.attr("src", e.target.result);
+          addMsg.hide();
+          imagesList.append(imageCtn);
+
+          imagesList.find(":nth-child(4n+3)").css("margin-left", "1.7em"); 
+          imagesList.find(":nth-child(4n+6)").css("margin-right", "1.7em");
+        };
+
+        fReader.readAsDataURL(file);
+      };
+
+      // Manage click event.
+      addIcon.on("click", function onClick(event) {
+        $(self).click();
+      });
+
+      dragbox.on("dragenter", function onDrop(event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        overlay.css("display", "flex");
+      });
+
+      dragbox.on("dragexit", function onDrop(event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        overlay.css("display", "none");
+      });
+
+      dragbox.on("dragleave", function onDrop(event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        overlay.css("display", "none");
+      });
+
+      dragbox.on("drop", function onDrop(event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        addIcon.css("margin-top", "1em");
+        overlay.css("display", "none");
+
+        var files = event.originalEvent.dataTransfer.files;
+        
+        for(var index = 0; index < files.length; ++index) {
+          readingFile(files[index]);
+        }
+      });
+
+
+      $(self).on("change", function() {
+        var files = this.files;
+
+        for(var index = 0; index < files.length; ++index) {
+          readingFile(files[index]);
+        }
+      });
+
+      $(self).hide();
+      dragbox.insertAfter(this);
 
     });
-    */
-   
+
     // Return "this" to ensure that chaining methods can be called.
     return this;
   };
