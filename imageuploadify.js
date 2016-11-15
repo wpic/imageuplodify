1070c823cd0386247770ab56d2d0393f933b2c73
Juliette TRAN <tran.juliette.a@gmail.com>
2016-11-15 10:56:20 +0800

    Add minify version, separate CSS from JS, clean code, fix issues, delete files, details on files...

--- imageuploadify.js
+++ imageuploadify.js
@@ -21,7 +21,7 @@
     e.preventDefault();
   }, false);
 
-  function compareMimeType(mimeTypes, fileType, formatFile) {
+  const compareMimeType = (mimeTypes, fileType, formatFile) => {
 
     // If accept is defined as *.
     if (mimeTypes.length < 2 && mimeTypes[0] === "*") {
@@ -32,28 +32,29 @@
     for (let index = 1; index < mimeTypes.length; index+=3) {
 
       // image/*, audio/*, video/*
-      if (mimeTypes[index + 1] === "*" && fileType.search(new RegExp(mimeTypes[index])) != -1) {
-        console.log("1");
+      if (mimeTypes[index + 1] === "*" &&
+        fileType.search(new RegExp(mimeTypes[index])) != -1) {
         return true;
       }
       // application/vnd.ms-excel, application/vnd.ms-powerpoint
-      else if (mimeTypes[index + 1] && mimeTypes[index + 1] != "*" && fileType.search(new RegExp("\\*" + mimeTypes[index + 1] + "\\*")) != -1) {
-        console.log("2");
+      else if (mimeTypes[index + 1] && mimeTypes[index + 1] != "*" &&
+        fileType.search(new RegExp("\\*" + mimeTypes[index + 1] + "\\*")) != -1) {
         return true;
       }
       // application/pdf, image/jpg
-      else if (mimeTypes[index + 1] && mimeTypes[index + 1] != "*" && fileType.search(new RegExp(mimeTypes[index + 1])) != -1) {
-        console.log("3");
+      else if (mimeTypes[index + 1] && mimeTypes[index + 1] != "*" &&
+        fileType.search(new RegExp(mimeTypes[index + 1])) != -1) {
         return true;
       }
       // .jpg, .pdf .png
-      else if (mimeTypes[index + 1] === "" && (fileType.search(new RegExp(mimeTypes[index])) != -1 || formatFile.search(new RegExp(mimeTypes[index])) != -1)) {
-        console.log("4");
+      else if (mimeTypes[index + 1] === "" &&
+        (fileType.search(new RegExp(mimeTypes[index])) != -1 || formatFile.search(new RegExp(mimeTypes[index])) != -1)) {
         return true;
       }
     }
     return false;
   }
+
   // Define the plugin imageuploadify.
   $.fn.imageuploadify = function(opts) {
 
@@ -71,22 +72,23 @@
         return;
       }
       // Save accept files
-      let accept = $(self).attr("accept").replace(/\s/g, "").split(",");
+      let accept = $(self).attr("accept") ? $(self).attr("accept").replace(/\s/g, "").split(",") : null;
       let result = [];
 
+      // Loop the array of accept files to split all part of mimetype or format.
       accept.forEach((item) => {
         let regexp;
+        // Select the regexp according to the result (mimetype or format)
         if (item.search(/\//) != -1) {
           regexp = new RegExp("([A-Za-z-.]*)\/([A-Za-z-*.]*)", "g");
         }
         else {
           regexp = new RegExp("\.([A-Za-z-]*)()", "g");
         }
+        // Exec the regexp and then 
         const r = regexp.exec(item);
         result = result.concat(r);
       });
-
-      console.log(result);
 
       // Array containing all files add by dialog box or drag'n drop.
       let totalFiles = [];
@@ -95,92 +97,50 @@
 
       // Define the dragbox layout.
       let dragbox = $(`
-      <div class="well">
-        <div class="overlay">
+      <div class="imageuploadify well">
+        <div class="imageuploadify-overlay">
         <i class="fa fa-picture-o"></i>
         </div>
-        <div class="images-list text-center">
+        <div class="imageuploadify-images-list text-center">
           <i class="fa fa-cloud-upload"></i>
-          <span>Drag & Drop Your File(s) Here To Upload</span>
+          <span class='imageuploadify-message'>Drag & Drop Your File(s) Here To Upload</span>
           <button type="button" class="btn btn-default">or select file to upload</button>
         </div>
       </div>
       `);
 
       // Save all elements of the dragbox.
-      let overlay = dragbox.find(".overlay");
-      let uploadIcon = dragbox.find(".overlay i");
+      let overlay = dragbox.find(".imageuploadify-overlay");
+      let uploadIcon = dragbox.find(".imageuploadify-overlay i");
 
-      let imagesList = dragbox.find(".images-list");
-      let addIcon = dragbox.find(".images-list i");
-      let addMsg = dragbox.find(".images-list span");
-      let button = dragbox.find(".images-list button");
-
-      // Dropbox CSS
-      dragbox.css("border", "2px dashed rgb(210, 210, 210)");
-      dragbox.css("position", "relative");
-      dragbox.css("min-height", "350px");
-      dragbox.css("min-width", "250px");
-      dragbox.css("max-width", "1000px");
-      dragbox.css("margin", "auto");
-      dragbox.css("display", "flex");
-      dragbox.css("padding", "0px");
-      dragbox.css("flex-direction", "column");
-      dragbox.css("text-align", "center");
-      dragbox.css("background-color", "white");
-      dragbox.css("color", "#3AA0FF");
-
-      // Overlay CSS
-      overlay.css("z-index", "10");
-      overlay.css("width", "100%");
-      overlay.css("height", "100%");
-      overlay.css("position", "absolute");
-      overlay.css("flex-direction", "column");
-      overlay.css("top", "0");
-      overlay.css("left", "0");
-      overlay.css("display", "none");
-      overlay.css("font-size", "7em");
-      overlay.css("background-color", "rgba(242, 242, 242, 0.7)");
-      overlay.css("text-align", "center");
-      overlay.css("pointer-events", "none");
-
-      // Upload icon CSS
-      uploadIcon.css("z-index", "10");
-      uploadIcon.css("position", "absolute");
-      uploadIcon.css("top", "50%");
-      uploadIcon.css("left", "50%");
-      uploadIcon.css("transform", "translate(-50%, -50%)");
-      uploadIcon.css("pointer-events", "none");
-
-      // Image list CSS
-      imagesList.css("display", "inline-block");
-
-      // Add icon CSS
-      addIcon.css("display", "block");
-      addIcon.css("font-size", "7em");
-      addIcon.css("text-align", "center");
-      addIcon.css("margin-top", "0.5em");
-      addIcon.css("padding-bottom", "12px");
-
-      // Add message CSS
-      addMsg.css("font-size", "24px");
-      addMsg.css("border-top", "1px solid #3AA0FF");
-      addMsg.css("border-bottom", "1px solid #3AA0FF");
-      addMsg.css("padding", "10px");
-      addMsg.css("display", "inline-block");
-
-      // Open dialog box button CSS
-      button.css("display", "block");
-      button.css("color", "#3AA0FF");
-      button.css("border-color", "#3AA0FF");
-      button.css("border-radius", "1em");
-      button.css("margin", "25px auto");
-      button.css("width", "100%");
-      button.css("max-width", "500px");
+      let imagesList = dragbox.find(".imageuploadify-images-list");
+      let addIcon = dragbox.find(".imageuploadify-images-list i");
+      let addMsg = dragbox.find(".imageuploadify-images-list span");
+      let button = dragbox.find(".imageuploadify-images-list button");
 
 
-      // Define the function to read a file.
-      const readingFile = (file) => {
+      /** FUNCTIONS  **/
+
+
+      // Function to read and store files. 
+      const retrieveFiles = (files) => {
+
+        for(let index = 0; index < files.length; ++index) {
+          if (!accept || compareMimeType(result, files[index].type, /(?:\.([^.]+))?$/.exec(files[index].name)[1])) {
+            // Unique number to save the image.
+            const id = Math.random().toString(36).substr(2, 9);
+
+            readingFile(id, files[index]);
+            totalFiles.push({
+              id:   id,
+              file: files[index]
+            });
+          }
+        }
+      }
+
+      // Function to read a file.
+      const readingFile = (id, file) => {
         const fReader = new FileReader();
 
         // Compute the number of box that could fit in the dragbox and the
@@ -190,44 +150,46 @@
         const marginSize = Math.floor((width - (boxesNb * 100)) / (boxesNb + 1));
 
         // Create the preview file container box.
-        let container = $("<div class='containerBox'></div>");
+        let container = $(`<div class='imageuploadify-container'>
+          <button type='button' class='btn btn-danger glyphicon glyphicon-remove'></button>
+          <div class='imageuploadify-details'>
+            <span>${file.name}</span>
+            <span>${file.type}</span>
+            <span>${file.size}</span>
+          </div>
+        </div>`);
+
+        let details = container.find(".imageuploadify-details");
+        let deleteBtn = container.find("button");
 
         // Preview file container box CSS
-        container.css("width", "100px");
-        container.css("height", "100px");
-        container.css("position", "relative");
-        container.css("overflow", "hidden");
         container.css("margin-left", marginSize + "px");
-        container.css("margin-bottom", "1em");
-        container.css("float", "left");
-        container.css("border-radius", "12px");
-        container.css("box-shadow", "0 0 4px 0 #888888");
 
+        // Manage display/hidding details about preview files.
+        details.hover(function() {
+          $(this).css("opacity", "1");
+        })
+        .mouseleave(function() {
+          $(this).css("opacity", "0");
+        });
+       
         // If the given file in the parameter is an image.
         if (file.type && file.type.search(/image/) != -1) {
           // Associated function to a ending load
           fReader.onloadend = function (e) {
             // Create the image tag for preview.
             let image = $("<img>");
-
-            // Image CSS
-            image.css("height", "100px");
-            image.css("left", "50%");
-            image.css("position", "absolute");
-            image.css("top", "50%");
-            image.css("transform", "translate(-50%, -50%)");
-            image.css("width", "auto");
-
             // Paste the image source to display the image preview.
             image.attr("src", e.target.result);
+
             // Append the image to its container and then the container to the
             // list of files.
             container.append(image);
             imagesList.append(container);
 
             // Apply left margin to first container of each row and right to last.
-            imagesList.find(".containerBox:nth-child(" + boxesNb + "n+4)").css("margin-left", marginSize + "px");
-            imagesList.find(".containerBox:nth-child(" + boxesNb + "n+3)").css("margin-right", marginSize + "px");
+            imagesList.find(".imageuploadify-container:nth-child(" + boxesNb + "n+4)").css("margin-left", marginSize + "px");
+            imagesList.find(".imageuploadify-container:nth-child(" + boxesNb + "n+3)").css("margin-right", marginSize + "px");
           };
 
         }
@@ -258,14 +220,52 @@
             imagesList.append(container);
 
             // Apply left margin to first container of each row and right to last.
-            imagesList.find(".containerBox:nth-child(" + boxesNb + "n+4)").css("margin-left", marginSize + "px");
-            imagesList.find(".containerBox:nth-child(" + boxesNb + "n+3)").css("margin-right", marginSize + "px");
+            imagesList.find(".imageuploadify-container:nth-child(" + boxesNb + "n+4)").css("margin-left", marginSize + "px");
+            imagesList.find(".imageuploadify-container:nth-child(" + boxesNb + "n+3)").css("margin-right", marginSize + "px");
           };
         }
+
+        // Delete the file from the list.
+        deleteBtn.on("click", function() {
+          $(this.parentElement).remove();
+          for (let index = 0; totalFiles.length > index; ++index) {
+            if (totalFiles[index].id === id) {
+              totalFiles.splice(index, 1);
+              break;    
+            }
+          }
+        });
 
         // Use the FileReader to read the content of a File.
         fReader.readAsDataURL(file);
       };
+
+      const disableMouseEvents = () => {
+        // Display the overlay and change the dragbox border color.
+        overlay.css("display", "flex");
+        dragbox.css("border-color", "#3AA0FF");
+
+        // Disable pointer events to avoid miscapture dragexit children's events.
+        button.css("pointer-events", "none");
+        addMsg.css("pointer-events", "none");
+        addIcon.css("pointer-events", "none");
+        imagesList.css("pointer-events", "none");
+      }
+
+      const enableMouseEvents = () => {
+          // Hide the overlay and put back the dragbox border color.
+          overlay.css("display", "none");
+          dragbox.css("border-color", "rgb(210, 210, 210)");
+
+        // Enable back pointer events to capture click, hover... 
+          button.css("pointer-events", "initial");
+          addMsg.css("pointer-events", "initial");
+          addIcon.css("pointer-events", "initial");
+          imagesList.css("pointer-events", "initial");
+      }
+
+      /** EVENTS  */
+
 
       // Change the color background of the button according to the mouse.
       button.mouseenter(function onMouseEnter(event) {
@@ -289,16 +289,7 @@
 
         // Increment the counter.
         counter++;
-
-        // Display the overlay and change the dragbox border color.
-        overlay.css("display", "flex");
-        dragbox.css("border-color", "#3AA0FF");
-
-        // Enable back pointer events to capture click, hover...
-        button.css("pointer-events", "none");
-        addMsg.css("pointer-events", "none");
-        addIcon.css("pointer-events", "none");
-        imagesList.css("pointer-events", "none");
+        disableMouseEvents();
       });
 
       // Manage events to hide the overlay when dragout files.
@@ -312,15 +303,7 @@
         // If the counter is equal to 0 (means that the files are entirely out
         // of the dragbox).
         if (counter === 0) {
-          // Hide the overlay and put back the dragbox border color.
-          overlay.css("display", "none");
-          dragbox.css("border-color", "rgb(210, 210, 210)");
-
-          // Disable pointer events to avoid miscapture dragexit children's events. 
-          button.css("pointer-events", "initial");
-          addMsg.css("pointer-events", "initial");
-          addIcon.css("pointer-events", "initial");
-          imagesList.css("pointer-events", "initial");
+          enableMouseEvents();
         }
       });
 
@@ -329,27 +312,13 @@
         event.stopPropagation();
         event.preventDefault();
 
-        // Hide the overlay and put back the dragbox border color.
-        overlay.css("display", "none");
-        dragbox.css("border-color", "rgb(210, 210, 210)");
-
-        // Enable back pointer events to capture click, hover...
-        button.css("pointer-events", "initial");
-        addMsg.css("pointer-events", "initial");
-        addIcon.css("pointer-events", "initial");
-        imagesList.css("pointer-events", "initial");
-
+        enableMouseEvents();
         // Retrieve the dragged files.
         const files = event.originalEvent.dataTransfer.files;
 
         // Read all files (to add them to the preview and push them to the files
         // list to submit).
-        for(let index = 0; index < files.length; ++index) {
-          if (!accept || compareMimeType(result, files[index].type, /(?:\.([^.]+))?$/.exec(files[index].name)[1])) {
-            readingFile(files[index]);
-            totalFiles.push(files[index]);
-          }
-        }
+        retrieveFiles(files);
       });
 
       // Binding resize event to the window.
@@ -368,16 +337,15 @@
             const marginSize = Math.floor((width - (boxesNb * 100)) / (boxesNb + 1));
 
             // Reset all margins of containers boxes.
-            let containers = imagesList.children(".containerBox");
+            let containers = imagesList.find(".imageuploadify-container");
             for (let index = 0; index < containers.length; ++index) {
               $(containers[index]).css("margin-right", "0px");
               $(containers[index]).css("margin-left", marginSize + "px");
             }
 
             // Apply left margin to first container of each row and right to last.
-            imagesList.find(".containerBox:nth-child(" + boxesNb + "n+4)").css("margin-left", marginSize + "px");
-            imagesList.find(".containerBox:nth-child(" + boxesNb + "n+3)").css("margin-right", marginSize + "px");
-
+            imagesList.find(".imageuploadify-container:nth-child(" + boxesNb + "n+4)").css("margin-left", marginSize + "px");
+            imagesList.find(".imageuploadify-container:nth-child(" + boxesNb + "n+3)").css("margin-right", marginSize + "px");
           }, 500);
         });
       })
@@ -386,13 +354,7 @@
       // and add them to the array.
       $(self).on("change", function onChange() {
         const files = this.files;
-
-        for(let index = 0; index < files.length; ++index) {
-          if (!accept || compareMimeType(result, files[index].type, /(?:\.([^.]+))?$/.exec(files[index].name)[1])) {
-            readingFile(files[index]);
-            totalFiles.push(files[index]);
-          }
-        }
+        retrieveFiles(files);
       });
 
       // When submitting the form.
@@ -425,7 +387,7 @@
 
         // Add all files get from the dialog box or drag'n drop,
         for (var i = 0; i < totalFiles.length; i++) {
-          formData.append(self.name, totalFiles[i]);
+          formData.append(self.name, totalFiles[i].file);
         }
 
         // Create an request and post all data.
@@ -445,14 +407,12 @@
         return false;
       });
 
+
       // Hide the original input.
       $(self).hide();
-
       // Insert the dragbox after the original hidden input.
       dragbox.insertAfter(this);
-
     });
-
     // Return "this" to ensure that chaining methods can be called.
     return this;
   };
